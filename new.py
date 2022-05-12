386e9c0587b5741df19560c120584a82b09b9fb3
chunkmaster420 <105442512+chunkmaster420@users.noreply.github.com>
2022-05-12 09:53:54 -0500

    Add files via upload

---
+++ new.py
@@ -0,0 +1,17 @@
+from subprocess import call
+from tkinter import N
+
+
+while True: 
+    poop = input("what would you like to say: ")
+    if poop=="hello":
+                print("hi") 
+    if poop!="hello":
+        print("fgo away") 
+    if poop=="lets talk about the political and economic state of the world":
+        print("go away jaden nobody loves you youre annoying")
+        if input("do you want to repeat (y/n)?") == "n": 
+            break
+
+        
+    
